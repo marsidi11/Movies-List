a53f8f09b8f4b1039ee2a1a0108eb1eb0e9debdc
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-04-14 18:03:49 +0200

    Design

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+>>> Poject Titile: Movies List
+>>> Start Date: 13.04.2019 
+>>> By: Marsid Zyberi
+*/
+
+function addmovie() {
+	var moviename = document.querySelector(".moviename").value;
+	
+}
