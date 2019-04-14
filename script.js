b8dd77c1fab3a24a301fa73094af2060b7a27612
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-04-14 19:22:52 +0200

    Add Movies

--- script.js
+++ script.js
@@ -4,7 +4,37 @@
 >>> By: Marsid Zyberi
 */
 
-function addmovie() {
-	var moviename = document.querySelector(".moviename").value;
+// "var ml" => movieslength
+
+var ml = 0;
+
+function addmovietitle() {
+	var moviename = document.getElementById("moviename").value.toUpperCase();
+	var createinput = document.createElement("input");
+	//alert(`Movies selected by you is "${moviename}"`);
+
 	
+	var x = document.getElementById("movies");
+	var y = x.getElementsByTagName("input")[ml];
+	y.value = moviename;
+	document.getElementById("moviename").value = ""
+
+	console.log(ml);
+	ml++;
 }
+
+function addmovie() {
+	addmovietitle()
+
+}	
+
+function keyDown() {
+	if (event.keyCode == 13) {
+		addmovietitle()
+	}
+}
+
+
+// Cookies
+
+document.cookie()
