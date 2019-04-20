1b3b2c44c1f3b0953a24280cca9063c574f83a46
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-04-20 15:51:35 +0200

    Local Storage
    Movies saved in local storage in PC,
     watched movies saved also

--- script.js
+++ script.js
@@ -8,8 +8,11 @@
 // "var pn" => printed number
 // "var nfr" => name for remove
 
-
 var ml = 0;
+
+var moviename = document.getElementById("moviename").value.toUpperCase();
+var x = document.getElementById("movies");
+var y = x.getElementsByTagName("input")[ml];
 
 function addmovietitle() {
 
@@ -17,22 +20,30 @@ function addmovietitle() {
 
 	var moviename = document.getElementById("moviename").value.toUpperCase();
 	var x = document.getElementById("movies");
+	var y = x.getElementsByTagName("input")[ml];
 
 	// Get input-s by Tag
-	var y = x.getElementsByTagName("input")[ml];
 	var pn = `${ml + 1}. `;
 
+
+	// ------------- Local Storage ----------------------------
+
+	localStorage.setItem("moviet" + ml, moviename);
+
 	// Change Values of input to Movie Title
+
 	y.value = pn + moviename;
+
 	document.getElementById("moviename").value = "";
 	document.getElementsByTagName("input")[ml + 2].style.visibility = "visible"
 	console.log(ml);
 	
-	ml++;
 
 	hidd();
-}
 
+	ml++;
+
+}
 
 // Prompt for watched movies
 // Change style of watched movies
@@ -42,7 +53,33 @@ function watched() {
 	var nfr = prompt("Which Movie Have You Watched? \nWrite Number!")
 	var y = x.getElementsByTagName("input")[nfr - 1];
 	y.style.color = "#1dd1a1";
-	y.style.textDecoration = "line-through"
+	y.style.textDecoration = "line-through";
+	// Local Storage
+
+	localStorage.setItem("color" + (nfr-1), "#1dd1a1");
+
+}
+
+// Set Movie Names to Box Values from Local Storage
+
+for (var i = 0; i < 100; i++) {
+	if (localStorage.getItem(`moviet${i}`) !== null) {
+
+		x.getElementsByTagName("input")[i].value = (i + 1) + (". ") + localStorage.getItem(`moviet${i}`)
+
+	}
+
+	if (localStorage.getItem("color" + i) == "#1dd1a1") {
+		x.getElementsByTagName("input")[i].style.color = "#1dd1a1"
+		x.getElementsByTagName("input")[i].style.textDecoration = "line-through"
+	}	
+
+	if (localStorage.getItem(`moviet${i}`) == null) {
+		console.log(i)
+		ml = i;
+		break
+	}
+
 }
 
 // When Input Box value = "" => set it to hidden
@@ -59,22 +96,24 @@ function hidd() {
 	}
 }
 
-// Cookies
-
-document.cookie = "moviename=" + document.getElementsByTagName("input").value;
-
-
-
 // Add Movie Button
 
 function addmovie() {
 	addmovietitle()
 }	
 
-// Enter Key for adding movie
+	// Enter Key for Adding Movie
 
 function keyDown() {
 	if (event.keyCode == 13) {
 		addmovietitle()
 	}
 }
+
+
+// Remove Local Storage
+
+/*for (var i = 0; i < 110; i++) {
+	localStorage.removeItem("moviet" + i)
+	localStorage.removeItem("color" + i)
+}*/
